115a78e95e84d745527523635e0c05ed3328efab
bogdan9876 <bogdankryveiko@gmail.com>
2025-02-09 19:10:37 +0200

    logo filename change

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -70,7 +70,7 @@ const Header = () => {
                     )}
                   >
                     <img
-                      src="/images/xora.svg"
+                      src="/images/logo.svg"
                       width={160}
                       height={55}
                       alt="logo"
