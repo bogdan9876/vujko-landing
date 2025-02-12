79367524be8a9cb66f29f7b451194aef8d90ac23
Bogdan <bogdankryveiko@gmail.com>
2025-02-13 01:46:47 +0200

    add temporary logo

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -39,9 +39,9 @@ const Header = () => {
         hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]",
       )}
     >
-      <div className="container flex h-14 items-center max-lg:px-5">
+      <div className="container flex h-14 items-center max-lg:px-6">
         <a className="lg:hidden flex-1 cursor-pointer z-2">
-          <img src="/images/xora.svg" width={115} height={55} alt="logo" />
+         <img src="favicon.ico" width={40} height={165} alt="logo" /> 
         </a>
 
         <div
