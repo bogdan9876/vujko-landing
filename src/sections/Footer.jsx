f675070c59401b6f0e015147877ae00aeb4bc730
Bogdan <bogdankryveiko@gmail.com>
2025-02-17 01:58:42 +0200

    add contacts info

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,14 +6,14 @@ const Footer = () => {
       <div className="container py-10">
         <div className="flex w-full max-md:flex-col">
           <div className="small-compact flex flex-1 flex-wrap items-center justify-center gap-5">
-            <p className="text-base opacity-70">Copyright, Vujko Systems</p>
+            <p className="text-xl">Контакти:</p>
           </div>
           <div className="flex items-center justify-center sm:ml-auto">
             <p className="legal-after relative mr-9 text-p5 transition-all duration-500 hover:text-p1">
-              Privacy Policy
+              +380 98 004 14 11
             </p>
             <p className="text-p5 transition-all duration-500 hover:text-p1">
-              Terms of Use
+              contact@vujko.net
             </p>
           </div>
 
