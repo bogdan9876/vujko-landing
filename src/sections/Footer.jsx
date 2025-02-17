78e6d22483548d3e3e865c538ece5a800c6428e3
bogdan9876 <bogdankryveiko@gmail.com>
2025-02-17 21:13:48 +0200

    return old footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -47,81 +47,37 @@ const Footer = () => {
 
   return (
     <footer>
-      <div className="container py-10">
-        <div className="flex w-full max-md:flex-col">
-          <div className="flex flex-1 flex-col items-center justify-center gap-5 max-md:w-full">
-            <div className="flex flex-col items-center gap-2">
-              <p className="text-xl text-aligh">Контакти:</p>
-              <p className="text-p5 transition-all duration-500 hover:text-p1 overflow-hidden whitespace-normal break-words">
-                +380 98 004 14 11
-              </p>
-              <p className="text-p5 transition-all duration-500 hover:text-p1">
-                contact@vujko.net
-              </p>
-            </div>
+    <div className="container py-10">
 
-            <ul className="flex flex-1 justify-center gap-3 max-md:mt-10 md:justify-end">
-              {socials.map(({ id, url, icon, title }) => (
-                <li key={id}>
-                  <a href={url} className="social-icon">
-                    <img
-                      src={icon}
-                      alt={title}
-                      className="size-3/5 object-contain"
-                    />
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <div className="flex flex-1 items-center justify-center max-md:w-full max-md:mt-10">
-            <form
-              onSubmit={handleSubmit}
-              className="flex flex-col gap-4 w-full max-w-md"
-            >
-              <h3 className="text-xl mb-5">Залишіть свої дані</h3>
-              <input
-                type="text"
-                name="name"
-                value={formData.firstName}
-                onChange={handleInputChange}
-                placeholder="Ваше ім'я"
-                className="p-3 border border-gray-300 rounded-xl cursor-pointer"
-                required
-              />
-              <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleInputChange}
-                placeholder="Ваша електронна пошта"
-                className="p-3 border border-gray-300 rounded-xl cursor-pointer"
-                required
-              />
-              <textarea
-                type="text"
-                name="text"
-                value={formData.text}
-                onChange={handleInputChange}
-                placeholder="Ваше запитання"
-                className="p-3 border border-gray-300 rounded-xl cursor-pointer resize-none"
-                required
-              />
-              <Button
-                type="submit"
-                className="mt-4 p-3 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-              >
-                Надіслати
-              </Button>
-              {statusMessage && (
-                <p className="mt-4 text-center text-lg">{statusMessage}</p>
-              )}
-            </form>
-          </div>
+      <div className="flex w-full max-md:flex-col">
+        <div className="small-compact flex flex-1 flex-wrap items-center justify-center gap-5">
+          <p className="text-xl">Контакти</p>
         </div>
+        <div className="flex items-center justify-center sm:ml-auto">
+          <p className="legal-after relative mr-9 text-p5 transition-all duration-500 hover:text-p1">
+          +380 98 004 14 11
+          </p>
+          <p className="text-p5 transition-all duration-500 hover:text-p1">
+          contact@vujko.net
+          </p>
+        </div>
+
+        <ul className="flex flex-1 justify-center gap-3 max-md:mt-10 md:justify-end">
+          {socials.map(({ id, url, icon, title }) => (
+            <li key={id}>
+              <a href={url} className="social-icon">
+                <img
+                  src={icon}
+                  alt={title}
+                  className="size-3/5 object-contain"
+                />
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
-    </footer>
+    </div>
+  </footer>
   );
 };
 
