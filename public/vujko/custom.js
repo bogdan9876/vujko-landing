1d5e424b495981f7d15ea7dc3d01ff7c69f3686c
bogdan9876 <bogdankryveiko@gmail.com>
2025-04-09 14:55:28 +0300

    update web config in confog section

--- public/vujko/custom.js
+++ public/vujko/custom.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
     if ('ontouchstart' in document.documentElement) {
         document.body.classList.add('touch');
     }
 
-    fetch('config/config.json')
-        .then(response => response.json())
-        .then(config => initializeConfig(config))
-        .catch(error => console.error("Error fetching config:", error));
+    loadConfig("config/config.json");
+
+    checkProfiles().then(() => {
+        loadDefaultProfile();
+    });
 
     document.getElementById("stationMineMode").addEventListener("change", toggleMineFields);
+    document.getElementById("accelMode").addEventListener("change", toggleDroneFields);
 
     document.addEventListener("submit", handleFormSubmit);
 
     showPage('timer');
-});
 
-document.addEventListener("DOMContentLoaded", () => {
     const explosionSlider = document.getElementById("explosionSlider");
     const explosionTimer = document.getElementById("explosionTimer");
 
@@ -24,8 +24,88 @@ document.addEventListener("DOMContentLoaded", () => {
     explosionSlider.addEventListener("input", function () {
         explosionTimer.textContent = this.value;
     });
+
+    syncValues();
+    syncExplosionTimer();
+
+    fetch("assets/tooltips.json")
+        .then(response => response.json())
+        .then(data => {
+            document.querySelectorAll(".tooltiptext[data-tooltip]").forEach(el => {
+                let key = el.getAttribute("data-tooltip");
+                if (data[key]) {
+                    el.textContent = data[key];
+                }
+            });
+        })
+        .catch(error => console.error("Помилка завантаження tooltips.json:", error));
+
+    document.getElementById("profileSelect").addEventListener("change", function () {
+        const profileNumber = this.value;
+        loadConfig(`assets/profile${profileNumber}.json`);
+        updateProfile();
+    });
+
+    document.getElementsByClassName("btn").addEventListener("click", function () {
+        const profileNumber = document.getElementById("profileSelect").value;
+        saveParameters(profileNumber);
+    });
 });
 
+function loadConfig(configPath) {
+    fetch(configPath)
+        .then(response => response.json())
+        .then(config => {
+            initializeConfig(config);
+            toggleLogButton();
+        })
+        .catch(error => console.error(`Помилка завантаження ${configPath}:`, error));
+}
+
+function syncValues(){
+    const slider = document.getElementById("readingThreshold");
+        const numberInput = document.getElementById("readingThresholdInput");
+        const valueDisplay = document.getElementById("thresholdValue");
+
+        function syncValues(source, target) {
+            target.value = source.value;
+            valueDisplay.textContent = source.value;
+        }
+
+        syncValues(slider, numberInput);
+
+        slider.addEventListener("input", function () {
+            syncValues(slider, numberInput);
+        });
+
+        numberInput.addEventListener("input", function () {
+            if (numberInput.value < slider.min) numberInput.value = slider.min;
+            if (numberInput.value > slider.max) numberInput.value = slider.max;
+            syncValues(numberInput, slider);
+        });
+}
+
+function syncExplosionTimer() {
+    const slider = document.getElementById("explosionSlider");
+    const numberInput = document.getElementById("explosionTimerInput");
+    const displayValue = document.getElementById("explosionTimer");
+
+    function sync(source, target) {
+        target.value = source.value;
+        displayValue.textContent = source.value;
+    }
+    sync(slider, numberInput);
+
+    slider.addEventListener("input", function () {
+        sync(slider, numberInput);
+    });
+
+    numberInput.addEventListener("input", function () {
+        if (numberInput.value < slider.min) numberInput.value = slider.min;
+        if (numberInput.value > slider.max) numberInput.value = slider.max;
+        sync(numberInput, slider);
+    });
+}
 
 function initializeConfig(config) {
     console.log("Config Loaded:", config);
@@ -33,22 +113,31 @@ function initializeConfig(config) {
     document.getElementById("name-logo").textContent = config['name-logo'];
     document.getElementById("img-logo").style.backgroundImage = `url(${config['img-logo']})`;
 
-    updateInputValue("setupTimer", config['Налаштування, сек']);
+    updateInputValue("setupTimer", config['Час очікування, сек']);
     updateInputValue("explosionTimer", config['До взриву, сек']);
     updateInputValue("protectionTimer", config['Захист після включення, сек']);
     updateInputValue("restartTimer", config['Час перезапуску, сек']);
 
+    document.getElementById("accelMode").checked = config['Акселерометр'] === true;
+    updateHiddenField('accelMode', 'accelModeHidden');
+
     const droneSettings = config['Поріг зчитання, G'];
     updateInputRange("readingThreshold", droneSettings);
+    document.getElementById("readingThresholdInput").value = droneSettings.value;
     document.getElementById("thresholdValue").textContent = droneSettings.value;
+
     updateInputValue("overloadExplosionThreshold", config['Взрив при перегрузці, G']);
-    document.getElementById("acDcModeToggle").checked = config['ac-dc-mode'] === 8;
+    
+    updateInputValue("pt1", config['pt1']);
+    updateInputValue("pt2", config['pt2']);
+    updateInputValue("filterRate", config['filterRate']);
+    updateInputValue("refreshRate", config['refreshRate']);
+    
+    // document.getElementById("acDcModeToggle").checked = config['ac-dc-mode'] === 8;
     document.getElementById("autoExplosionToggle").checked = config['Автовибух по таймеру'] === 1;
-    document.getElementById("serverDroneMode").checked = config['Режим сервера'] === true;
-    updateHiddenField('serverDroneMode', 'serverDroneModeHidden');
     document.getElementById("logsDroneEnabled").checked = config['Логування даних'] === true;
     updateHiddenField('logsDroneEnabled', 'logsDroneEnabledHidden');
-
+    
     document.getElementById("stationMineMode").checked = config['Режим міни'];
     updateInputValue("mineActivityThreshold", config['Поріг Активності, G']);
     updateInputValue("mineInactivityThreshold", config['Поріг Бездіяльності, G']);
@@ -57,11 +146,13 @@ function initializeConfig(config) {
     updateInputValue("mineTapDuration", config['Загальна тривалість дотику, мс']);
     updateInputValue("mineTapDelay", config['Загальна затримка дотику, мс']);
 
-    updateSliderValue("explosionSlider", "explosionTimer");
-    document.getElementById("explosionSlider").value = config['До взриву, сек'];
-    document.getElementById("explosionTimer").textContent = config['До взриву, сек'];
+    const explosion = config['До взриву, сек'];
+    updateInputValue("explosionTimerInput", explosion);
+    updateInputRangeExplosion("explosionSlider", explosion);
+    document.getElementById("explosionTimer").textContent = explosion;
 
     toggleMineFields();
+    toggleDroneFields();
 }
 
 function updateInputValue(id, value) {
@@ -79,14 +170,13 @@ function updateInputRange(id, settings) {
     }
 }
 
-function updateSliderValue(sliderId, displayId) {
-    const slider = document.getElementById(sliderId);
-    const display = document.getElementById(displayId);
-    if (slider && display) {
-        display.textContent = slider.value;
-        slider.addEventListener('input', () => {
-            display.textContent = slider.value;
-        });
+function updateInputRangeExplosion(id, value) {
+    const element = document.getElementById(id);
+    const valueDisplay = document.getElementById('explosionTimer');
+
+    if (element && value) {
+        element.value = value;
+        valueDisplay.textContent = value;
     }
 }
 
@@ -94,6 +184,12 @@ function toggleMineFields() {
     const isMineModeActive = document.getElementById("stationMineMode").checked;
     const mineFields = document.getElementById("mine-fields");
     mineFields.style.display = isMineModeActive ? "block" : "none";
+}
+
+function toggleDroneFields() {
+    const isDroneModeActive = document.getElementById("accelMode").checked;
+    const droneFields = document.getElementById("drone-fields");
+    droneFields.style.display = isDroneModeActive ? "block" : "none";
 }
 
 function handleFormSubmit(e) {
@@ -120,7 +216,7 @@ function handleFormSubmit(e) {
 
 function mapFormDataToConfig(formData) {
     const fieldMapping = {
-        'setupTimer': 'Налаштування, сек',
+        'setupTimer': 'Час очікування, сек',
         'explosionTimer': 'До взриву, сек',
         'protectionTimer': 'Захист після включення, сек',
         'restartTimer': 'Час перезапуску, сек',
@@ -133,10 +229,14 @@ function mapFormDataToConfig(formData) {
         'mineTapThreshold': 'Загальний поріг дотику, G',
         'mineTapDuration': 'Загальна тривалість дотику, мс',
         'mineTapDelay': 'Загальна затримка дотику, мс',
+        'accelModeHidden': 'Акселерометр',
         'acDcModeHidden': 'ac-dc-mode',
         'autoExplosionHidden': 'Автовибух по таймеру',
         'logsDroneEnabledHidden': 'Логування даних',
-        'serverDroneModeHidden': 'Режим сервера'
+        'pt1': 'pt1',
+        'pt2': 'pt2',
+        'filterRate': 'filterRate',
+        'refreshRate': 'refreshRate'
     };
 
     const mappedData = {};
@@ -160,7 +260,7 @@ function handleFormResponse(data, form) {
 
 function showPage(pageId) {
     event.preventDefault();
-
+    
     const pages = document.querySelectorAll('.content-page');
     pages.forEach(page => page.classList.add('hide'));
 
@@ -181,16 +281,28 @@ function showToast(message) {
     setTimeout(() => toast.classList.remove("show"), 2500);
 }
 
+function reloadOptions() {
+    fetch('http://192.168.4.1/reload')
+        .then(response => response.text())
+        .then(data => showToast(data === "Options loaded" ? "Опції успішно завантажено!" : "Помилка при завантаженні опцій"))
+        .catch(error => {
+            console.error("Error:", error);
+            showToast("Помилка при завантаженні опцій");
+        });
+}
 
-function saveParameters() {
+function saveParameters(profileNumber) {
     const configData = {
+        "name-logo": "Vujko WebServer",
+        "img-logo": "/config/logo.txt",
         "param-box3": "Відлік часу",
-        "Налаштування, сек": parseInt(document.getElementById('setupTimer').value),
-        "До взриву, сек": parseInt(document.getElementById('explosionTimer').value),
+        "Час очікування, сек": parseInt(document.getElementById('setupTimer').value),
+        "До взриву, сек": parseInt(document.getElementById('explosionSlider').value),
         "Захист після включення, сек": parseInt(document.getElementById('protectionTimer').value),
         "Час перезапуску, сек": parseInt(document.getElementById('restartTimer').value),
         "param-box8": "Дрон",
-        "ac-dc-mode": document.getElementById('acDcModeToggle').checked ? 8 : 0,
+        "Акселерометр": document.getElementById('accelModeHidden').value === "1",
+        "ac-dc-mode": 0,
         "Поріг зчитання, G": {
             value: parseFloat(document.getElementById('readingThreshold').value),
             min: 1,
@@ -199,9 +311,12 @@ function saveParameters() {
         },
         "Взрив при перегрузці, G": parseFloat(document.getElementById('overloadExplosionThreshold').value),
         "Автовибух по таймеру": parseInt(document.getElementById('autoExplosionHidden').value),
+        "pt1": parseFloat(document.getElementById('pt1').value),
+        "pt2": parseFloat(document.getElementById('pt2').value),
+        "filterRate": parseFloat(document.getElementById('filterRate').value),
+        "refreshRate": parseFloat(document.getElementById('refreshRate').value),
         "Логування даних": document.getElementById('logsDroneEnabledHidden').value === "1",
-        "Режим сервера": document.getElementById('serverDroneModeHidden').value === "1",
-        "param-box15": "Міна",
+        "param-box19": "Міна",
         "Режим міни": document.getElementById('stationMineModeHidden').value === "1",
         "Поріг Активності, G": parseFloat(document.getElementById('mineActivityThreshold').value),
         "Поріг Бездіяльності, G": parseFloat(document.getElementById('mineInactivityThreshold').value),
@@ -209,27 +324,30 @@ function saveParameters() {
         "Загальний поріг дотику, G": parseFloat(document.getElementById('mineTapThreshold').value),
         "Загальна тривалість дотику, мс": parseInt(document.getElementById('mineTapDuration').value),
         "Загальна затримка дотику, мс": parseInt(document.getElementById('mineTapDelay').value),
-        "name-logo": "Vujko WebServer",
-        "img-logo": "/config/img-logo-128_128.txt"
     };
 
     const configJSON = JSON.stringify(configData, null, 2);
     const configBlob = new Blob([configJSON], { type: "application/json" });
+    const sendConfig = (filePath) => {
     const formData = new FormData();
-    formData.append("data", configBlob, '/config/config.json');
+    formData.append("data", configBlob, filePath);
 
-    fetch('/edit', {
+    return fetch('/edit', {
         method: 'POST',
         body: formData
+    });
+};
 
-    })
-        .then(response => response.text())
-        .then((responseText) => {
-            showToast(`Файл успішно збережено: ${responseText}`);
-        })
-        .catch((error) => {
-            showToast("Помилка при збереженні конфігурації!");
-        });
+Promise.all([
+    sendConfig(`assets/profile${profileNumber}.json`),
+    sendConfig(`config/config.json`)
+])
+.then(() => {
+    showToast("Файли успішно збережені!");
+})
+.catch(() => {
+    showToast("Помилка при збереженні конфігурації!");
+});
 }
 
 function updateHiddenField(checkboxId, hiddenFieldId) {
@@ -241,36 +359,113 @@ function updateHiddenField(checkboxId, hiddenFieldId) {
 }
 
 document.getElementById("reset-defaults").addEventListener("click", () => {
-    const defaultConfig = {
-        "param-box3": "Відлік часу",
-        "Налаштування, сек": 120,
-        "До взриву, сек": 1200,
-        "Захист після включення, сек": 10,
-        "Час перезапуску, сек": 30,
-        "param-box8": "Дрон",
-        "ac-dc-mode": 0,
-        "Поріг зчитання, G": {
-            "value": 10,
-            "min": 1,
-            "max": 16,
-            "step": 0.1
-        },
-        "Взрив при перегрузці, G": 15,
-        "Автовибух по таймеру": false,
-        "Логування даних": true,
-        "Режим сервера": false,
-        "param-box15": "Міна",
-        "Режим міни": false,
-        "Поріг Активності, G": 4,
-        "Поріг Бездіяльності, G": 2,
-        "Час Бездіяльності, сек": 5,
-        "Загальний поріг дотику, G": 3.5,
-        "Загальна тривалість дотику, мс": 30,
-        "Загальна затримка дотику, мс": 100,
-        "name-logo": "Vujko WebServer",
-        "img-logo": "/config/img-logo-128_128.txt"
+    fetch("assets/defaultConfig.json")
+        .then(response => response.json())
+        .then(config => {
+            initializeConfig(config);
+            const configBlob = new Blob([JSON.stringify(config, null, 2)], { type: "application/json" });
+            const profileIndex = document.getElementById("profileSelect").value;
+            const formData = new FormData();
+            formData.append("data", configBlob, `assets/profile${profileIndex}.json`);
+        
+            fetch('/edit', {
+                method: 'POST',
+                body: formData
+        
+            })
+            .then(response => response.text())
+            .then((responseText) => {
+                showToast(`Параметри скинуті до значень за замовчуванням! ${responseText}`);
+                loadConfig(`assets/profile${profileIndex}.json`);
+            })
+            .catch((error) => {
+                showToast("Помилка при збереженні конфігурації!");
+            });
+        })
+        .catch(error => {
+            showToast("Помилка при завантаженні конфігурації!");
+        });
+});
+
+var port = location.port || (window.location.protocol === 'https:' ? '443' : '80');
+var esp = `${window.location.protocol}//${window.location.hostname}:${port}/`;
+
+function restartESP() {
+    fetch(esp + "reset")
+    .then(response => response.text())
+    .then(data => {
+        console.log("done")
+    });
+}
+
+function toggleLogButton() {
+    let checkbox = document.getElementById('logsDroneEnabled');
+    let button = document.getElementById('logButton');
+    let hiddenInput = document.getElementById('logsDroneEnabledHidden');
+    
+    if (checkbox.checked) {
+        button.style.display = 'inline-block';
+        hiddenInput.value = '1';
+    } else {
+        button.style.display = 'none';
+        hiddenInput.value = '0';
+    }
+}
+
+function updateProfile() {
+    document.getElementById('profile-version').textContent = 
+        "Профіль: " + document.getElementById('profileSelect').value;
+}
+
+function checkProfiles() {
+    const profileSelect = document.getElementById("profileSelect");
+    const profileFolder = 'assets/';
+    let totalProfiles = 0;
+
+    profileSelect.innerHTML = '';
+
+    const checkProfile = async (i) => {
+        const profilePath = `${profileFolder}profile${i}.json`;
+
+        try {
+            const response = await fetch(profilePath, { method: "GET" });
+            if (response.ok) {
+                totalProfiles++;
+                const option = document.createElement('option');
+                option.value = i;
+                option.textContent = `Профіль ${i}`;
+                profileSelect.appendChild(option);
+            }
+        } catch {
+            console.log("Помилка при перевірці профілю");
+        }
     };
 
-    initializeConfig(defaultConfig);
-    showToast("Параметри скинуті до значень за замовчуванням!");
-});
+    const profilePromises = [];
+    for (let i = 1; i <= 4; i++) {
+        profilePromises.push(checkProfile(i));
+    }
+    return Promise.all(profilePromises);
+}
+
+function loadDefaultProfile() {
+    const profileSelect = document.getElementById("profileSelect");
+    if (profileSelect.options.length > 0) {
+        profileSelect.selectedIndex = 0;
+        updateProfile();
+        loadConfig(`assets/profile${profileSelect.value}.json`);
+    }
+}
+
+function toggleFilters() {
+    const filters = document.getElementById('filters');
+    const filtersToggle = document.getElementById('filters-toggle');
+
+    if (filters.style.display === 'none' || filters.style.display === '') {
+        filters.style.display = 'block';
+        filtersToggle.innerText = 'Сховати фільтри';
+    } else {
+        filters.style.display = 'none';
+        filtersToggle.innerText = 'Показати фільтри';
+    }
+}
