88d5a270fe64c9a55cd11c1e6152759ac7004ca3
bogdan9876 <bogdankryveiko@gmail.com>
2025-04-20 15:36:43 +0300

    website

--- src/sections/WebSite.jsx
+++ src/sections/WebSite.jsx
@@ -18,34 +18,9 @@ const WebSite = () => {
                   Сайт конфігурації
                 </h3>
               </div>
-
               <p className="body-1 mb-10 max-w-md">
-                Адаптивний сайт конфігурації під будь-який пристрій
+                Адаптивний сайт конфігурації під будь-який пристрій. Можна конфігурувати: перезагрузку, таймери, підрив, вмикати та вимикати режим міни.
               </p>
-
-              <ul className="flex flex-wrap items-center gap-4">
-                {links.map(({ id, url, icon }) => (
-                  <li
-                    key={id}
-                    className="download_tech-link download_tech-link_last-before download_tech-link_last-after"
-                  >
-                    <a
-                      href={url}
-                      className="size-22 download_tech-icon_before relative flex items-center justify-center rounded-half border-2 border-s3 bg-s1 transition-borderColor duration-500"
-                    >
-                      <span className="absolute -top-2 rotate-90">
-                        <Marker />
-                      </span>
-                      <img
-                        src={"/images/lines.svg"}
-                        alt="lines"
-                        className="absolute size-13/20 object-contain"
-                      />
-                      <span className="download_tech-icon">{icon}</span>
-                    </a>
-                  </li>
-                ))}
-              </ul>
             </div>
 
             <div className="mb-10 max-md:hidden">
